b6b1062f174c8622420941b6807ac7f24038d064
Marcin Płaza <121696948+MarcinPlaza1@users.noreply.github.com>
2024-09-17 22:06:13 +0200

    upgrade

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
-// index.js
-const { createServer } = require('http');
-const config = require('./config'); // Importuje wszystko z config/index.js
-const configureExpress = require('./config/express'); // Importuje konfigurację Express
+import config from './config/index.js';
+import configureExpress from './config/express.js';
 
 const startServer = async () => {
   try {
-    // Połączenie z MongoDB
     await config.connectMongoDB();
-
-    // Tworzenie instancji aplikacji Express z skonfigurowanymi middleware'ami
     const app = configureExpress();
 
-    // Tworzenie serwera HTTP
-    const httpServer = createServer(app);
+    const { server, httpServer } = await config.setupGraphQL(app);
 
-    // Konfiguracja Apollo Server i WebSocket
-    const server = await config.setupGraphQL(app, httpServer);
-
-    // Uruchomienie serwera
     const PORT = process.env.PORT || 4000;
     httpServer.listen(PORT, () => {
-      config.logger.info(`🚀 Serwer działa na http://localhost:${PORT}${server.graphqlPath}`);
-      config.logger.info(`🚀 Subskrypcje działają na ws://localhost:${PORT}${server.graphqlPath}`);
+      config.logger.info(
+        `🚀 Serwer działa na http://localhost:${PORT}${server.graphqlPath}`
+      );
+      config.logger.info(
+        `🚀 Subskrypcje działają na ws://localhost:${PORT}${server.subscriptionsPath}`
+      );
     });
   } catch (err) {
     config.logger.error('Błąd podczas uruchamiania serwera', err);
